7fed97a995e21483591eab2e6ed1489a1c712bd0
cjgochanour <cjgochanour@gmail.com>
2022-02-25 10:02:20 -0600

    place-names js does what csharp code does

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,12 +1,27 @@
 // Put your code here
 
 const names = [
-   "Nashville", "Hong Kong", "The back yard", "Earth",
-    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
+    "Nashville",
+    "Hong Kong",
+    "The back yard",
+    "Earth",
+    "London",
+    "The mall",
+    "Ryman Auditorium",
+    "The Great Wall of China",
 ];
 
 console.log("All Pace Names");
 
 for (const name of names) {
-  console.log(name);
+    console.log(name);
+}
+
+console.log("");
+console.log("'The' Place Names");
+
+for (const name of names) {
+    if (name.startsWith("The")) {
+        console.log(name);
+    }
 }
